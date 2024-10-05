e89dd7e6a564cbf7723731baec46c3753cdcc522
ajnash.k02@gmail.com <ajnash.k02@gmail.com>
2024-10-05 08:40:36 +0530

    fix build issue

--- client/src/app/dashboard/booking/page.tsx
+++ client/src/app/dashboard/booking/page.tsx
@@ -31,20 +31,8 @@ import {
 } from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
 import { Form } from "react-hook-form";
-export enum BookingStatus {
-  INIT = 1,
-  FORM_COMPLETED = 2,
-  PAYMENT_FAILED = 3,
-  PAYMENT_SUCCESS = 4,
-  ADMIN_APPROVED = 5,
-  CHECK_IN = 6,
-}
-export enum BedAvailabilityStatus {
-  AVAILABLE = 0,
-  ENGAGED = 1,
-  OCCUPIED = 2,
-  NOT_AVAILABLE = 3,
-}
+import { BedAvailabilityStatus, BookingStatus } from "./_lib/enums";
+
 
 export type Booking = {
   _id: string;
